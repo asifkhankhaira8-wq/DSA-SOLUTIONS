a0841b7bcc2405a88ecdcfc6ff40f9373d43bbda
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-07-04 15:09:37 +0530

    Time: 12 ms (33.60%) | Memory: 11.1 MB (51.64%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+      int n=s.size();
+      int left=0;
+      int right=0;
+      int cnt=0;
+      int freq[3]={0};
+      for(int i=0;i<n;i++){
+         freq[s[i]-'a']++;
+         while(freq[0]>=1 && freq[1]>=1 && freq[2]>=1){
+            cnt+=(n-i);
+            freq[s[left]-'a']--;
+            left++;
+         }
+      }
+      return cnt;
+    }
+};
