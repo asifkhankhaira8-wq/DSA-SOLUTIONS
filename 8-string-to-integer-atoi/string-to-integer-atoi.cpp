e415aa30c053bbcc0518e8b1cc8dacc99a5bc298
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-07-06 15:42:42 +0530

    Time: 4 ms (5.82%) | Memory: 10.3 MB (5.63%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+int fun(int i,string s,int sign,long long &num){
+        int n = s.size();
+        if (i == n)  return 0;
+        if(!isdigit(s[i])) return 0;
+        int digit = s[i] - '0';
+       if (num > (INT_MAX - digit) / 10) {
+             return (sign == 1) ? INT_MAX : INT_MIN;
+         }
+          num=digit+num*10;
+        return fun(i+1,s,sign,num);
+
+
+
+}
     int myAtoi(string s) {
         int n = s.size();
         int i = 0;
-        while (i < n && s[i] == ' ')
-            i++;
+        while (i < n && s[i] == ' ')  i++;
         if (i == n)
             return 0;
         int sign = 1;
@@ -14,18 +27,10 @@ public:
         } else if (s[i] == '+') {
             i++;
         }
-        long long num = 0;
-        while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-
-            if (num > (INT_MAX - digit) / 10) {
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-
-            num = num * 10 + digit;
-            i++;
-        }
-
-        return sign * num;
+        long long num=0;
+       
+         int val=fun(i,s,sign ,num);
+         if(val==INT_MAX || val==INT_MIN) return val;
+        return sign *num;
     }
 };
