96343f570adab950d7348477e58d13d10987eee8
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-07-30 23:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (94.72%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -21,8 +21,17 @@ public:
         preorder(root->right,ans);
      }
     vector<int> preorderTraversal(TreeNode* root) {
+        if(root==NULL) return {};
          vector<int>ans;
-        preorder(root,ans);
+         stack<TreeNode*>st;
+         st.push(root);
+         while(!st.empty()){
+            TreeNode* top=st.top();
+            st.pop();
+            ans.push_back(top->val);
+            if(top->right) st.push(top->right);
+            if(top->left) st.push(top->left);
+         }
         return ans;
     }
 };
