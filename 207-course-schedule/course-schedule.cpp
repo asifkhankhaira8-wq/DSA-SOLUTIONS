e6e512e61ea503ed87acb83b7a0548848f535b8f
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-08-20 19:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (45.28%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+bool dfs(int node, vector<vector<int>>&adj,vector<int>&vis,vector<int>&pathVis){
+    vis[node]=1;
+    pathVis[node]=1;
+    for(auto it : adj[node]){
+        if(!vis[it]){
+            if(dfs(it,adj,vis,pathVis)){
+                return true;
+            }}
+            else if( pathVis[it]){
+                   return true;
+            }
+    }
+    pathVis[node]=0;
+    return false;
+}
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        const int n=numCourses;
+      vector<int> pathVis(n,0);
+       vector<int> vis(n,0);
+        for(int i=0;i<numCourses;i++){
+           if(!vis[i]){
+            if(dfs(i,adj,vis,pathVis)) return false;
+           }
+        }
+        return true;
+    }
+};
