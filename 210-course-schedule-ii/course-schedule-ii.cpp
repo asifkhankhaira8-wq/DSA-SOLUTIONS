c4c665e8be1e692c607d44ead5875a096c12ec9f
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-08-21 19:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (71.24%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-vector<int>ans;
-bool dfs(int node, vector<vector<int>>&adj,vector<int>&vis,vector<int>&pathVis){
-    vis[node]=1;
-    pathVis[node]=1;
-    for(auto it : adj[node]){
-        if(!vis[it]){
-            if(dfs(it,adj,vis,pathVis)){
-                return true;
-            }}
-            else if( pathVis[it]){
-                   return true;
-            }
-    }
-    ans.push_back(node);
-    pathVis[node]=0;
-    return false;
-}
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-          vector<vector<int>>adj(numCourses);
+         vector<vector<int>>adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
             int u=prerequisites[i][0];
             int v=prerequisites[i][1];
             adj[v].push_back(u);
         }
-        const int n=numCourses;
-      vector<int> pathVis(n,0);
-       vector<int> vis(n,0);
+        vector<int>indegree(numCourses);
+        queue<int>q;
         for(int i=0;i<numCourses;i++){
-           if(!vis[i]){
-            if(dfs(i,adj,vis,pathVis)) return {};
-           }
+            for(int it:adj[i]){
+                indegree[it]++;
+            }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
+         int top=q.front();
+         q.pop();
+            ans.push_back(top);
+            for(int it:adj[top]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+   if(ans.size()==numCourses)  return  ans ;
+   return {};
     }
 };
