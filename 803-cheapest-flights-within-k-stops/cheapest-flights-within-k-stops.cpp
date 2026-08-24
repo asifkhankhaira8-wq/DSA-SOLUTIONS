e0df8cf6517f6bbf5c7992110c3902afb9995492
asifkhankhaira8-wq <asifkhankhaira8@gmail.com>
2026-08-24 18:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (73.36%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -10,34 +10,28 @@ public:
             adj[u].push_back({v,w});
         }
 
-        vector<vector<int>> dist(n, vector<int>(k+2, 1e9));
-        dist[src][0]=0;
-
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-
-        pq.push({0,{src,0}});
-    
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{src,0}});
+        while(!q.empty()){
+             auto it=q.front();
+             q.pop();
             int w=it.first;
             int node=it.second.first;
-            int d=it.second.second;
+            int key=it.second.second;
 
-            if(node == dst){
-                return w;
-            }
-
-            if(d>k) continue;
+            if(key>k) continue;
             for(auto neigh:adj[node]){
-                if(w+neigh.second<dist[neigh.first][d+1]){
-                    dist[neigh.first][d+1]=w+neigh.second;
-                    pq.push({dist[neigh.first][d+1],{neigh.first,d+1}});
+                if(w+neigh.second<dist[neigh.first]){
+                    dist[neigh.first]=w+neigh.second;
+                    q.push({dist[neigh.first],{neigh.first,key+1}});
                 }
             }
 
         }
-        return  -1 ;
+        if(dist[dst]==1e9) return -1;
+        return dist[dst];
+
     }
 };
